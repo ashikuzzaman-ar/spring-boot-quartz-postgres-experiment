7e8516da24efe2daca2ff717c92440f379311523
Ashikuzzaman <ashikuzzaman.ar@gmail.com>
2022-05-16 10:59:35 +0600

    Add new exception with ResponseStatus
    - Add InternalServerErrorException with ResponseStatus of HttpStatus.INTERNAL_SERVER_ERROR

---
+++ src/main/java/dev/ashikuzzaman/experiment/springboot_quartz_postgres_experiment/exception/InternalServerErrorException.java
@@ -0,0 +1,8 @@
+package dev.ashikuzzaman.experiment.springboot_quartz_postgres_experiment.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerErrorException extends RuntimeException {
+}
